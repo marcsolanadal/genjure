0a3719060a59d2d785fd8eb4a8fb17eb2732de98
Marc Solà Nadal <ionthasMail@gmail.com>
2015-05-23 00:05:15 +0200

    Basic functions finished and added some documentation for them.

--- src/genjure/core.clj
+++ src/genjure/core.clj
@@ -1,6 +1,24 @@
 (ns genjure.core
   (:gen-class))
 
+;; This will define the encoding of the hole GA.
+;; If we want to solve simple GA we should use binary encoding, permutation
+;; encoding or value encoding.
+;;
+;; Binary encoding = [1 1 1 0 0]
+;; Permutation encoding = [4 6 3 9 7 5]
+;; Value encoding = [red black red red black]
+
+;; For mor advanced uses such as GP we should use tree encoding.
+;;
+;; Tree encoding = (+ (- 2 4) 5)
+
+(defn new-gene [] (* (rand) 1))
+
+
+;; This crossover function is independent of the encoding we are using.
+;; It has the strenght of being able to change the mask vector. If we do so, we
+;; can explore how different crossover points affect the performance of our GA.
 
 (defn crossover
   "Produces a new vector based on the genotypes [gt1 gt2] taking the mask as
@@ -8,59 +26,79 @@
   to [gt1]."
   [gt1 gt2 mask]
   (let [inv-mask (mapv #(- 1 %) mask)
-        masked-gt1 (mapv * gt1 mask)
-        masked-gt2 (mapv * gt2 inv-mask)]
+        masked-gt1 (mapv * gt1 inv-mask)
+        masked-gt2 (mapv * gt2 mask)]
     (mapv + masked-gt1 masked-gt2)))
 
-(crossover [1 2 3 4 5]
-           [9 8 7 6 0]
-           [0 1 0 1 0])
-
-
-;; TODO: The mimimal swap window size is 2 genes. Maybe in the future that can
-;; be improved to support 1 gene swaps.
-;; FIXME: Strange behaviour regarding the hold=false transition. It seems that
-;; something is delaying the application of the value from g1->g2.
-
-;; The cross-points are needed to be inside a '(). Ex. (crossover g1 g2 '(2 5))
-
-(defn crossover
-  [genotype1 genotype2 cross-points]
-  (loop [gene 0
-         new-genotype []
-         points cross-points
-         hold false]
-    (if (= gene (count genotype1))
-      (do
-        (printf "%s = [%s, %s, %s]\n" gene new-genotype points hold)
-        new-genotype)
-      (if (= gene (first points))
-        (do
-          (printf "%s = [%s, %s, %s]\n" gene new-genotype points hold)
-          (recur (inc gene) (conj new-genotype (nth genotype2 gene)) (pop points) (not hold)))
-        (if (true? hold)
-          (do
-            (printf "%s = [%s, %s, %s]\n" gene new-genotype points hold)
-            (recur (inc gene) (conj new-genotype (nth genotype2 gene)) points hold))
-          (do
-            (printf "%s = [%s, %s, %s]\n" gene new-genotype points hold)
-            (recur (inc gene) (conj new-genotype (nth genotype1 gene)) points hold)))))))
+(defn mutate
+  "Takes a genotype [gt] and modifies it based on the mutation-rate [rate].
+  The rate input value must range between 0 and 1."
+  [gt rate]
+  (loop [n 0 mutated-gt gt]
+    (if (= n (count gt))
+      mutated-gt
+      (if (<= (rand) rate)
+        (recur (inc n) (assoc mutated-gt n (new-gene)))
+        (recur (inc n) mutated-gt)))))
 
 (defn generate-genotype
-  [length]
-  (loop [n 0
-         genotype []]
-    (if (= n length)
+  "Generates a new genotype of a given length [len] with the gene structure
+  defined in the (new-gene) function."
+  [len]
+  (loop [n 0 genotype []]
+    (if (= n len)
       genotype
-      (recur (inc n) (conj genotype (rand-int 10))))))
+      (recur (inc n) (conj genotype (new-gene))))))
 
 (defn generate-population
-  [size length]
+  "Generates a population of the specified number of genotypes [size]. Each
+  genotype consisting in a chain of genes with length [len]."
+  [size len]
   (loop [n 0
          population []]
     (if (= n size)
       population
-      (recur (inc n) (conj population (generate-genotype length))))))
+      (recur (inc n) (conj population (generate-genotype len))))))
+
+;; Steady-state selection
+;; ----------------------
+;; Only the best individuals are selected and breeded. The worst individuals are
+;; are discardted and substituted for the new ones.
+
+;; Rulette wheel selection
+;; -----------------------
+;; The better chromosomes are, the more chances to be selected they have.
+;; This method have problems when the fitnesses differs very much.
+
+;; Rank selection
+;; --------------
+;; First all population is ranked with his fitness function. Then, the worst
+;; individuals are assigned with the lower fitness and the better individuals
+;; are assigned with the greater fitness.
+;;
+;; Ex. Worst individual = 1
+;;     Second worst infividual = 2
+;;     Best indivudual = n
+
+;; Elitism
+;; -------
+;; The best chromosomes are selected and copyed to the next generation.
+;; This will be applyied before every crossover and mutation.
+
+(defn tournament-selection
+  [population size p]
+  (loop [n 0 contenders []]
+    (if (= n size)
+      (let [p (rand)]
+        (cond
+          ()
+          )
+        )
+
+      )
+    )
+  ;; Select random individuals
+  )
 
 (defn evolve
   [population generation]
@@ -71,13 +109,41 @@
         (evaluate
           (mutation
             (crossover
-              (select-best)))))
+              (tournament-selection)))))
       (dec generation))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (use 'criterium.core)
+
+  ;; Sample genotypes for performance testings
+  (def gt1 (generate-genotype 10))
+  (def gt2 (generate-genotype 10))
+
+  ;; (with-progress-reporting (bench
+  ;;                          (crossover gt1 gt2 [1 1 1 0 0 1 0 1 0 0])
+  ;;                           :verbose))
+  ;; 25.55us
+
+  ;; (with-progress-reporting (bench
+  ;;                           (mutate [1 1 1 1 1 1 1 1 1 1] 0.1)
+  ;;                           :verbose))
+  ;; 1.52us
+
+  ;;(defn generate-genotype1
+  ;;  [len]
+  ;;  (mapv #(* % (new-gene)) (vec (replicate len 1))))
+
+  ;; (with-progress-reporting (bench (generate-genotype 1000) :verbose))
+  ;; 542us
+
+  ;; (with-progress-reporting (bench (generate-genotype2 10) :verbose))
+  ;; 2.85us
+
+)
 
 
 
--- test/genjure/core_test.clj
+++ test/genjure/core_test.clj
@@ -32,13 +32,13 @@
     (let [g1 [1 2 3 4 5 6 7 8 9 0]
           g2 [0 9 8 7 6 5 4 3 2 1]]
       (is
-        (vector? (crossover g1 g2 [5]))
+        (vector? (crossover g1 g2 [1 1 1 1 1 0 0 0 0 0]))
         "should return a new vector genotype.")
       (is
-        (= [1 2 8 7 6 5 4 3 2 1 0] (crossover g1 g2 [2]))
+        (= [0 9 8 7 6 6 7 8 9 0] (crossover g1 g2 [1 1 1 1 1 0 0 0 0 0]))
         "should be able to cross genotypes with one cross-point.")
       (is
-        (= [1 2 8 7 5 6 4 3 9 0] (crossover g1 g2 [2 4 6 8]))
+        (= [0 9 3 4 6 5 4 8 2 0] (crossover g1 g2 [1 1 0 0 1 1 1 0 1 0]))
         "should be able to cross genotypes with multiple cross-points."))))
 
 
